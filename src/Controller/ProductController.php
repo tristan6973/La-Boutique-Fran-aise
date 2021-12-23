184aafe8730b2f60c0a698d9bfde4687b17dd678
Hichem Marih <hichem.marih@hotmail.com>
2021-12-23 13:15:35 +0100

    Version qui fonctionne avec toute les fonctionnalités jusqu'à la dernière vidéo de (5. Les produits)

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\From\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,27 +22,38 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
 
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
 
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
+        else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
         return $this->render('product/index.html.twig',[
-            'products' => $products
+            'products' => $products,
+            'form' => $form->CreateView()
         ]);
     }
 
     /**
      * @Route("/produit/{slug}", name="product")
      */
-    public function show($slug): Response
+    public function show($slug)
     {
         $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
-        if (!$product){
+        //$products = $this->entityManager->getRepository(Product::class)->findByIsBest(1);
+        if (!$product) {
             return $this->redirectToRoute('products');
         }
 
-        return $this->render('product/show.html.twig',[
+        return $this->render('product/show.html.twig', [
             'product' => $product
         ]);
     }
